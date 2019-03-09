2cad709a9e21c3e925ec3c20a7ae563c25327a46
Lykourgos Tsirikos <lykourgos.tsirikos@gmail.com>
2019-03-09 12:45:07 +0000

    Move alerts into app layout

--- application/resources/views/dashboard.blade.php
+++ application/resources/views/dashboard.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div class="row justify-content-center">
     <div class="col-md-8">
-        @include('layouts.partials.alerts._alerts')
         <div class="card">
             <div class="card-header">Dashboard</div>
 
--- application/resources/views/layouts/partials/alerts/_alerts.blade.php
+++ application/resources/views/layouts/partials/alerts/_alerts.blade.php
@@ -1,11 +1,11 @@
 @if(session()->has('success'))
-    @component('layouts.partials.alerts._alert_component', ['type' => 'success'])
+    @component('layouts.partials.alerts._alert_component', ['type' => 'success', 'icon' => 'check-circle'])
         {{ session('success') }}
     @endcomponent
 @endif
 
 @if(session()->has('error'))
-    @component('layouts.partials.alerts._alert_component', ['type' => 'danger'])
+    @component('layouts.partials.alerts._alert_component', ['type' => 'danger', 'icon' => 'exclamation-circle'])
         {{ session('error') }}
     @endcomponent
 @endif
