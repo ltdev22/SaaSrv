1bd14c9c8c55265c77fdce1a7e3219cb84b6b6b2
Lykourgos Tsirikos <lykourgos.tsirikos@gmail.com>
2019-03-21 22:02:27 +0000

    Two Factor: Create countries table

---
+++ application/app/Models/Country.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SaaSrv\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    /**
+     * Remove default timestamps.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
---
+++ application/database/migrations/2019_03_21_215252_create_countries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('iso_code_2', 5);
+            $table->string('iso_code_3', 5);
+            $table->string('dial_code', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
